060a04ef5a4127937ccf4c795ca87111d92a412c
byshlata <byshlata@yandex.ru>
2021-12-01 00:31:14 +0300

    просто  так

--- test.js
+++ test.js
@@ -1,3 +1,53 @@
-squirrel=(h,H,S)=>+(H/h*(h*h+S*S)**(1/2)).toFixed(4)
+function sumIntervals(intervals){
+    
+    intervals.sort(compare);
+    console.log (intervals);
+    for (let i = 0; i < intervals.length - 1; i++){
+        if (intervals[i][0] == intervals[i+1][0] && intervals[i][1] == intervals[i+1][1]){
+        intervals.splice (i+1, 1);
+        i--;
+    }
+}
+for (let i = 0; i < intervals.length - 1; i++){
+    if (intervals[i][0] < intervals[i+1][0] && intervals[i][1] > intervals[i+1][1]){
+        intervals.splice (i+1, 1);
+        i--;
+    } 
+    } 
 
-console.log (squirrel(4,16,3));
+for (let i = 0; i < intervals.length - 1; i++){
+    if (intervals[i][0] > intervals[i+1][0] && intervals[i][1] < intervals[i+1][1]){
+        intervals[i][1] = intervals[i+1][1];
+        intervals.splice (i+1, 1);
+        i--;
+    } 
+    }    
+  
+    for (let i = 0; i < intervals.length - 1; i++){
+        if (intervals[i][0] <= intervals[i+1][0] && intervals[i][1] >= intervals[i+1][0]){
+            intervals[i][1] = intervals[i+1][1];
+            intervals.splice (i+1, 1);
+            i--;
+        } 
+        }   
+
+        let summ = 0;
+        for (let i = 0; i < intervals.length; i++){
+            summ += intervals[i][1] - intervals[i][0];        
+        } 
+
+console.log (intervals);
+    return summ;
+}
+
+function compare(a, b){
+        if (a[0] == b[0]){
+            return +a[1] - b[1];
+        } else {
+          return +a[0] - b[0];  
+        }
+      
+    }
+
+
+console.log(sumIntervals([ [ 2, 9 ], [ 2, 6 ], [ 2, 4 ], [ 2, 9 ], [ 2, 5 ] ]));
