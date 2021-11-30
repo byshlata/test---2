d0a001587e32168450f1954157101143874d4e53
byshlata <byshlata@yandex.ru>
2021-12-01 01:41:12 +0300

    Игра с красными и желтыми кружечками типа массива 6х7

--- test.js
+++ test.js
@@ -1,53 +1,64 @@
-function sumIntervals(intervals){
-    
-    intervals.sort(compare);
-    console.log (intervals);
-    for (let i = 0; i < intervals.length - 1; i++){
-        if (intervals[i][0] == intervals[i+1][0] && intervals[i][1] == intervals[i+1][1]){
-        intervals.splice (i+1, 1);
-        i--;
-    }
-}
-for (let i = 0; i < intervals.length - 1; i++){
-    if (intervals[i][0] < intervals[i+1][0] && intervals[i][1] > intervals[i+1][1]){
-        intervals.splice (i+1, 1);
-        i--;
-    } 
-    } 
-
-for (let i = 0; i < intervals.length - 1; i++){
-    if (intervals[i][0] > intervals[i+1][0] && intervals[i][1] < intervals[i+1][1]){
-        intervals[i][1] = intervals[i+1][1];
-        intervals.splice (i+1, 1);
-        i--;
-    } 
-    }    
-  
-    for (let i = 0; i < intervals.length - 1; i++){
-        if (intervals[i][0] <= intervals[i+1][0] && intervals[i][1] >= intervals[i+1][0]){
-            intervals[i][1] = intervals[i+1][1];
-            intervals.splice (i+1, 1);
-            i--;
-        } 
-        }   
-
-        let summ = 0;
-        for (let i = 0; i < intervals.length; i++){
-            summ += intervals[i][1] - intervals[i][0];        
-        } 
-
-console.log (intervals);
-    return summ;
-}
-
-function compare(a, b){
-        if (a[0] == b[0]){
-            return +a[1] - b[1];
-        } else {
-          return +a[0] - b[0];  
-        }
+function connectFour(board) {
+    let index = ['draw'];
+    let indexR = 0;
+    let indexY = 0;
+  for (let i = 0; i < board.length; i++){
       
+    for (let k =0 ; k < board[0].length; k++){
+        if ((k < 4) && (board[i][k] == board[i][k+1]) && (board[i][k+2] == board[i][k+3]) && (board[i][k+1] == board[i][k+2]) && (board[i][k] != '-')){
+            if (board[i][k] == 'R'){
+                indexR++;
+            } else {
+                indexY++;
+            }
+        }
+        if ((i < 3) && (board[i][k] == board[i+1][k]) && (board[i+2][k] == board[i+3][k]) && (board[i+1][k] == board[i+2][k]) && (board[i][k] != '-')){
+            if (board[i][k] == 'R'){
+                indexR++;
+            } else {
+                indexY++;
+            }
+        }
+
+        if ((i < 3) && (k <4) && (board[i][k] == board[i+1][k+1]) && (board[i+2][k+2] == board[i+3][k+3]) && (board[i+1][k+1] == board[i+2][k+2]) && (board[i][k] != '-')){
+            if (board[i][k] == 'R'){
+                indexR++;
+            } else {
+                indexY++;
+            }
+        }
+
+        if ((i < 3) && (k > 2) && (board[i][k] == board[i+1][k-1]) && (board[i+2][k-2] == board[i+3][k-3]) && (board[i+1][k-1] == board[i+2][k-2]) && (board[i][k] != '-')){
+            if (board[i][k] == 'R'){
+                indexR++;
+            } else {
+                indexY++;
+            }
+        }
+        if (board[i][k] == '-'){
+          index[0] = 'in progress';
+      }
     }
+  }
+ if (indexY > indexR){
+     return 'Y';
+ } else {
+     if (indexR > indexY){
+         return 'R';
+     } else {
+         if (indexY == indexR){
+             return 'draw';
+         } else {
+             return index[0];
+         }
+     }
+ }
+}
 
 
-console.log(sumIntervals([ [ 2, 9 ], [ 2, 6 ], [ 2, 4 ], [ 2, 9 ], [ 2, 5 ] ]));
+console.log(connectFour([['R','R','R','Y','-','-','-'],
+                         ['R','R','Y','-','-','-','-'],
+                         ['R','Y','-','-','-','-','-'],
+                         ['Y','-','-','-','-','-','R'],
+                         ['-','-','-','-','-','R','R'],
+                         ['-','-','-','-','R','R','R']]));
